feafa9c74887443d890f96b2f3b43dc917e41fb7
Waldyr de Souza <waldyr.ar@gmail.com>
2016-07-22 00:04:44 -0300

    Tests

--- spec/balboa_spec.rb
+++ spec/balboa_spec.rb
@@ -6,4 +6,8 @@ describe Balboa do
   it 'has a version number' do
     expect(Balboa::VERSION).not_to be nil
   end
+
+  it 'has a target host' do
+    expect(Balboa::HOST).not_to be nil
+  end
 end
